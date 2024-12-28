b02c4fc7bf10bfc03b47e595afdcba8e5b2ab011
Alao Zubair Lanre <alaozubair@gmail.com>
2024-12-28 21:30:59 +0100

    new naija deli crazy updates

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,5 +1,6 @@
 // export const dynamic = 'force-dynamic'
-export const revalidate = 1;
+import User from "@/app/lib/models/users";
+
 import { connectToDB } from "@/utils/database";
 import Post from "@/app/lib/models/posts";
 import { NextRequest, NextResponse } from 'next/server';
@@ -8,9 +9,22 @@ export const GET = async (request: NextRequest): Promise<NextResponse> => {
     try {
         await connectToDB();
 
-        const posts = await Post.find({}).populate('creator');
+        const posts = await Post.find({}).populate({
+            path: "creator"
+          });
 
-        return new NextResponse(JSON.stringify(posts), { status: 200 })
+          
+          const response =  new NextResponse(JSON.stringify(posts), { status: 200 })
+
+         // Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(request.url);
+        url.searchParams.set("t", Date.now().toString());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+
+        return response;
     } catch (error) {
         return new NextResponse("Failed to fetch all posts", { status: 500 })
     }
