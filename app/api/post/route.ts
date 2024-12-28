db89adce3e508cdac5bb65ac42537bcebf76df5e
Alao Zubair Lanre <alaozubair@gmail.com>
2024-12-28 20:57:29 +0100

    new naija deli addd relatidate

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,5 +1,5 @@
 // export const dynamic = 'force-dynamic'
-
+export const revalidate = 0;
 import { connectToDB } from "@/utils/database";
 import Post from "@/app/lib/models/posts";
 import { NextRequest, NextResponse } from 'next/server';
